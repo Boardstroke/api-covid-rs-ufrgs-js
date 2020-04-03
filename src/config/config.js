76e1e87e9e7d882cba662ecfa1ca10725a1ebaff
Boardstroke <matheus.fachinis@gmail.com>
2020-04-03 04:24:51 -0300

    adding gitignore and enviroments variables

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,13 @@
 const path = require("path");
 module.exports = {
-  port: "8081",
+  port: process.env.PORT || "8081",
   db: {
     name: "api-covid-ufrgs",
-    user: "admin",
-    password: "admin",
+    user: process.env.DB_USER || "admin",
+    password: process.env.DB_PASS || "admin",
     options: {
-      dialect: "sqlite",
-      host: "localhost",
+      dialect: process.env.DIALECT || "sqlite",
+      host: process.env.HOST || "localhost",
       storage: path.join(__dirname, "../../api-covid-ufrgs.sqlite")
     }
   }
